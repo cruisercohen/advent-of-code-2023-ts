353e13a15123dc52691ddc1f1dfe896ab3a09109
cruiser <1767162-cruiser@users.noreply.replit.com>
2024-12-03 01:47:25 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { day1, day1part2 } from "./avent01";
+import { a01 } from "./2024/advent01";
+
+//2023
+// console.log(`day1 part 1: ${day1()}`);
+// console.log(`day1 part 2: ${day1part2()}`);
+
+//2024
+a01();
